f5f286ae8130731361441955e7e6deca4663e339
Jay Cheroske <jay@portal.org>
2019-06-22 19:03:30 -0700

    working on publish script; added prettier

---
+++ bin/publish.js
@@ -0,0 +1,11 @@
+const {version} = require('../package.json')
+
+const FACTOMD_DOCKER_TAGS=[
+  "FD-689_test0", "v6.3.1-rc1" "v6.3.1-rc2" "v6.3.1" "v6.3.1-rc1-anchors" "v6.3.2-rc3" "v6.3.2"
+  ]
+
+for tag in "${TAGS[@]}"
+do
+   docker build --build-arg FACTOMD_TAG=${tag} -t bedrocksolutions/factomd:${tag} .
+   docker push bedrocksolutions/factomd:${tag}
+done
